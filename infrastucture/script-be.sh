4913953819a56d6f95a440bf34ee340e442a41f8
Yash Thakkar <65316816+yash-thakkar248@users.noreply.github.com>
2021-03-21 15:07:10 -0400

    changes in scripts

--- infrastucture/script-be.sh
+++ infrastucture/script-be.sh
@@ -4,7 +4,7 @@ sudo apt install nginx -y
 sudo apt-get install git -y
 sudo apt update
 sudo apt-get update
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 git clone https://github.com/yash-thakkar248/UberBusApp.git
 cd UberBusApp/uber-backend/
 pip3 install -r requirements.txt
