1237d70aca7eb8f7c12580cb425d29f76eb81051
Yash Thakkar <yashthakkar@Yashs-MacBook-Pro.local>
2021-03-22 00:36:56 -0400

    added services for nginx

--- infrastucture/script-be.sh
+++ infrastucture/script-be.sh
@@ -1,11 +1,37 @@
 #! /bin/bash
+# sudo apt update
+# sudo apt install nginx -y
+# sudo apt-get install git -y
+# sudo apt update
+# sudo apt-get update
+# sudo apt install python3-pip -y
+# git clone https://github.com/yash-thakkar248/UberBusApp.git
+# cd UberBusApp/uber-backend/
+# pip3 install -r requirements.txt
+# # python3 uberbe.py
+
 sudo apt update
 sudo apt install nginx -y
 sudo apt-get install git -y
 sudo apt update
-sudo apt-get update
 sudo apt install python3-pip -y
+sudo pip3 install virtualenv
+virtualenv .venv
+source .venv/bin/activate
 git clone https://github.com/yash-thakkar248/UberBusApp.git
-cd UberBusApp/uber-backend/
-pip3 install -r requirements.txt
-# python3 uberbe.py
+pip3 install -r UberBusApp/uber-backend/requirements.txt
+pip uninstall bson -y
+pip uninstall pymongo -y
+pip3 install bson
+pip3 install pymongo
+pip3 install gunicorn
+sudo cp wsgi.py UberBusApp/uber-backend/
+sudo cp back-end.service /etc/systemd/system/back-end.service 
+sudo systemctl daemon-reload
+sudo systemctl start back-end
+sudo systemctl enable back-end
+sudo systemctl daemon-reload
+sudo cp back-end.nginx /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/back-end.nginx /etc/nginx/sites-enabled/back-end.nginx
+sudo systemctl reload nginx
--- infrastucture/script-fe.sh
+++ infrastucture/script-fe.sh
@@ -1,103 +1,20 @@
 #! /bin/bash
 sudo apt update
-# sudo apt install unzip -y
+sudo apt install unzip -y
 sudo apt install nginx -y
 sudo apt-get install git -y
+sudo apt-get update
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt update
 sudo apt install npm -y
-# sudo apt install python3-pip -y
 git clone https://github.com/yash-thakkar248/UberBusApp.git
 cd UberBusApp/uber-frontend/
-npm install
-# npm start
-# pip3 install -r requirements.txt
-# sudo apt install python3-pip -y
-# sudo apt install python3 -y
-# python3 --version
-# sudo apt install unzip -y
-# sudo apt update
-# sudo apt install nginx -y
-# sudo apt-get install python3-venv -y
-# sudo apt-get install git
-# git clone https://github.com/yash-thakkar248/UberBusApp.git
-# ls
-# sudo apt-get install node
-# sudo apt-get install nodejs
-# ls
-# cd UberBusApp/
-# ls
-# cd uber-frontend/
-# ls
-# npm install
-# sudo apt install npm
-# npm install
-# npm start
-# npm install
-# npm start
-# npm start eslint-disable-next-line
-# ls
-# npm install
-# npm audit fix
-# npm start
-# exit
-# ls
-# cd UberBusApp/
-# ls
-# cd uber-backend/
-# ls
-# python3 uberbe.py 
-# cd ..
-# ls
-# mkdir yash
-# cd yash/
-# ls
-# git clone https://github.com/yash-thakkar248/UberBusApp.git
-# ls
-# cd UberBusApp/
-# ls 
-# cd uber-backend/
-# ls
-# pip install requirements.txt 
-# pip3 install requirements.txt 
-# sudo apt install pip3
-# sudo apt install pip
-# sudo apt install python-pip
-# ls
-# python get-pip.py
-# python3 get-pip.pyD
-# python3 --version
-# curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-# pip3 install requirements.txt 
-# pip install requirements.txt 
-# sudo apt install python3-pip
-# pip install requirements.txt 
-# pip3 install requirements.txt 
-# ls
-# pip3 install requirements.txt
-# pip3 install -r requirements.txt
-# python3 get-pip.py
-# python3 uberbe.py 
-# vim uberbe.py 
-# python3 uberbe.py 
-# vim uberbe.py 
-# python3 uberbe.py 
-# netstat
-# sudo apt install net-tools
-# netstat
-# netstat -lntup
-# python -m venv c:\path\to\myenv
-# python3 -m venv c:\path\to\myenv
-# pip install virtualenv
-# apt install virtualenv
-# sudo apt install virtualenv
-# virtualenv mypython
-# source mypython/bin/activate
-# pip3 install -r requirements.txt
-# python3 uberbe.py 
-# vim uberbe.py 
-# python3 uberbe.py 
-# vim uberbe.py 
-# python3 uberbe.py 
-# vim uberbe.py 
-# python3 uberbe.py 
+sudo npm install
+sudo npm run-script build
+cd
+sudo cp front-end.nginx /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/front-end.nginx /etc/nginx/sites-enabled/front-end.nginx
+sudo systemctl reload nginx
